d10d2036055cf6ea0b682e739bc19fb495578572
gq <935338441@qq.com>
2022-10-22 11:59:43 +0800

    first commit

---
+++ js/about.d6ed8aac.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkxz"]=self["webpackChunkxz"]||[]).push([[443],{7381:function(n,u,t){t.r(u),t.d(u,{default:function(){return l}});var a=t(3396);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var i=t(89);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.d6ed8aac.js.map
